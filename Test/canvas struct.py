b6e085ecca6e73fbb152a62da68ce93f975705d1
theScrlLkKey <microman5252@gmail.com>
2024-11-27 23:48:32 -0800

    test works

--- Test/canvas struct.py
+++ Test/canvas struct.py
@@ -8,10 +8,15 @@ def fast_output(text):
 
 
 def output_canvas():
-    pass  # update canvas using fastoutput, iterate through and parse into outputtable chars/locations
+    # update canvas using fastoutput, iterate through and parse into outputtable chars/locations
+    for y in range(40):
+        for x in range(100):
+            fast_output(f"\033[{y};{x}H")
+            fast_output(f"\033[{canvas[y][x]}m█")
+        fast_output("\n")
 
 
-# setup canvas, fill with blanks, 40x100 canvas for now
+# setup canvas, fill with blanks, 40x100 canvas for now, replace with var in actual code, make init_canvas()
 canvas = []
 for y in range(40):
     row = []
@@ -20,6 +25,7 @@ for y in range(40):
     canvas.append(row)
 
 print(canvas)
+output_canvas()
 
 # rand color canvas
 for y in range(40):
@@ -27,3 +33,4 @@ for y in range(40):
         canvas[y][x] = str(random.randint(90, 97))
 
 print(canvas)
+output_canvas()
