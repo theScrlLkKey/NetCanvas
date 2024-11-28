a83d436ff8ad91bc23f896fca47360a30f4664da
theScrlLkKey <microman5252@gmail.com>
2024-11-28 05:45:21 -0800

    added secondary color

--- CLI/climain.py
+++ CLI/climain.py
@@ -26,13 +26,16 @@ def output_canvas(rows=40, columns=100):
     fast_output(output_str)
 
 
-def output_pixel(row=20, column=50, color="34", mode="solid"):  # use for cursor and also individual pixel update
+def output_pixel(row=20, column=50, color="37", mode="solid", text="null", show_col=False):  # use for cursor and also individual pixel update
     if mode == "solid":
-        fast_output(f"\033[{row};{(column*2)-1}H\033[{canvas[row-1][column-1]}m██")  # -1 because terminal indexes at 1
+        fast_output(f"\033[{row};{(column*2)-1}H\033[{color}m██")  # -1 because terminal indexes at 1
     elif mode == "trans":
-        fast_output(f"\033[{row};{(column*2)-1}H\033[{canvas[row-1][column-1]}m▒▒")
+        if show_col:
+            fast_output(f"\033[{row};{(column * 2) - 1}H\033[{color}m▒\033[{curs_color}m▒")
+        else:
+            fast_output(f"\033[{row};{(column * 2) - 1}H\033[{color}m▒▒")
     elif mode == "str":
-        fast_output(f"\033[39m\033[{row};{(column*2)-1}H{str(color)}")
+        fast_output(f"\033[39m\033[{row};{(column*2)-1}H\033[{color}m{text}")
 
 
 def generate_canvas(rows=40, columns=100):
@@ -50,11 +53,10 @@ def randomize_canvas(rows=40, columns=100):
     global canvas
     for y in range(rows):
         for x in range(columns):
-            colors = [30,31,32,33,34,35,36,37,90,91,92,93,94,95,96,97]
             canvas[y][x] = str(colors[random.randint(0, 15)])
 
 
-def change_character(row=20, column=50, color="34"):
+def change_character(row=20, column=50, color="37"):
     canvas[row][column] = color
     # pixel send code here
 
@@ -62,14 +64,17 @@ def change_character(row=20, column=50, color="34"):
 def on_press(key):
     global curs_x
     global curs_y
-    # 1-9 here
+    global curs_color
+    # 1-5, q-t, a-g for primary colors, 6-0, y-p, h-; for secondary colors
     if key == Key.space:  # set color
-        change_character(curs_y-1, curs_x-1, "34")  # -1 because curs_xy is terminal (index at 1) and canvas indexes at 0
+        change_character(curs_y-1, curs_x-1, curs_color)  # -1 because curs_xy is terminal (index at 1) and canvas indexes at 0
+    elif key == Key.shift:  # set color (secondary)
+        change_character(curs_y - 1, curs_x - 1, curs_sec_color)
     elif key == Key.left:
         if curs_x > 1:
             curs_x -= 1
     elif key == Key.right:
-        if curs_x < canvas_cols:  # cursor is beeg, remeber
+        if curs_x < canvas_cols:
             curs_x += 1
     elif key == Key.up:
         if curs_y > 1:
@@ -81,7 +86,7 @@ def on_press(key):
         fast_output("\033[2J ")
         _thread.interrupt_main()
         exit()
-    output_pixel(1, canvas_cols+1, str(curs_y) + ", " + str(curs_x) + "  ", mode="str")
+    output_pixel(1, canvas_cols+1, "37", "str", str(curs_y) + ", " + str(curs_x) + "  ")  # move to statusbar
 
 
 def start_key_press():
@@ -101,12 +106,18 @@ fast_output("\033[1;1H")  # calib cursor location
 curs_x = 1
 curs_y = 1
 
-output_pixel(canvas_rows+1, 1, "<keybinds>", mode="str")
+colors = [30,31,32,33,34,35,36,37,90,91,92,93,94,95,96,97]
+
+curs_color = "33"
+curs_sec_color = "90"
+
+output_pixel(canvas_rows+1, 1, "37", "str", "<keybinds>")
 
 key_press_sub = threading.Thread(target=start_key_press, args=())
 key_press_sub.start()  # send updates here
 
 while True:  # begin mainloop, get updates here
     output_canvas(canvas_rows, canvas_cols)   # get changed pixels and update those in canvas before redraw
-    output_pixel(curs_y, curs_x, canvas[curs_y-1][curs_x-1], "trans")  # update virtual cursor, can't desync, -1 yk
+    output_pixel(curs_y, curs_x, canvas[curs_y-1][curs_x-1], "trans")  # update virtual cursor, can't desync
+    # statusbar: keybinds + cursor pos (placeholder) + bold current color/italic secondary color in keybinds
     time.sleep(0.1)
