621f790029b5449eee8172f234e38fb12ae587db
theScrlLkKey <microman5252@gmail.com>
2024-11-27 23:35:06 -0800

    nmessin around

--- Test/canvas struct.py
+++ Test/canvas struct.py
@@ -1,4 +1,5 @@
 import sys
+import random  # colored static sometime
 
 
 def fast_output(text):
@@ -6,4 +7,16 @@ def fast_output(text):
     sys.stdout.flush()
 
 
+def output_canvas():
+    pass  # update canvas using fastoutput, iterate through and parse into outputtable chars/locations
+
+
+canvas[10][20] = "30"
+print(canvas)
+# setup canvas, fill with blanks, 40x100 canvas for now
 canvas = []
+for y in range(40):
+    for x in range(100):
+        canvas[y][x] = "30"
+
+print(canvas)
